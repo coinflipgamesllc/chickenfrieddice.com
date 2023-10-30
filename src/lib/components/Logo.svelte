9e1d849418b6173122b05934813449512cf36cf3
Robert Newton <robert.newton11@gmail.com>
2023-10-30 14:31:04 -0700

    initial commit

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,15 @@
+<script>
+	// import { onMount } from 'svelte';
+	// import { Motion, useCycle } from 'svelte-motion';
+
+	// let i = useCycle(-6, 6);
+	// let speed = 12;
+	// onMount(() => {
+	// 	const interval = setInterval(() => {
+	// 		i.next();
+	// 	}, speed * 1000);
+	// 	return () => clearInterval(interval);
+	// });
+</script>
+
+<img src="logo.svg" class="w-1/3" alt="Chicken Fried Dice Logo" />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Logo from '../lib/components/Logo.svelte';
+</script>
+
+<svelte:head>
+	<title>Chicken Fried Dice!</title>
+	<link rel="icon" type="image/svg" href="/favicon.ico" />
+</svelte:head>
+
+<div class="container mx-auto w-full flex flex-col items-center justify-center">
+	<Logo />
+	<div
+		class="w-full mx-auto md:p-8 p-4 bg-white/[0.8] border-4 border-black md:border-b-0 rounded flex flex-col items-center text-center"
+	>
+		<h1 class="text-4xl">More details coming soon!</h1>
+		<h2 class="text-2xl">Sign up for our newsletter</h2>
+		<p class="text-xl">Be the first to hear about Chicken Fried Dice updates!</p>
+
+		<script
+			async
+			src="https://eocampaign1.com/form/2f3eefe6-7763-11ee-a449-41078a26d697.js"
+			data-form="2f3eefe6-7763-11ee-a449-41078a26d697"
+		></script>
+
+		<img src="/customers.png" alt="Customer cards showcase" class="customers md:inline hidden" />
+	</div>
+</div>
+
+<footer class="-mt-40">
+	<img src="/dice.png" alt="Dice footer" />
+</footer>
